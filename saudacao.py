a606200ac1ee451d69c542297f30d9fed3425b74
joão <joao.c.silva11@edu.senai.br>
2026-05-26 08:05:27 -0300

    add funcao de soma

--- saudacao.py
+++ saudacao.py
@@ -1,5 +1,7 @@
 def saudacao(nome):
-  return f'tenha um otimo dia, {nome} '
->>>>> saudacao
+ returnf' tenha um otimo de semana, {nome}
 
-print(saudacao('joao'))
+def soma (n1,n2):
+  resultado = n1 = n2
+return(resultado)
+ print(saudacao('joão'))
